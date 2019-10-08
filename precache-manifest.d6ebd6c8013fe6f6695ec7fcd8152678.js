8d806bb31b9ba200bcb0da0c4709347cb96161d6
Alex Andrutskii <alex.andrutskii@gmail.com>
2019-10-08 15:05:58 +0300

    Updates

---
+++ precache-manifest.d6ebd6c8013fe6f6695ec7fcd8152678.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0c349c8f21cf98ed4f9356b1e972202",
+    "url": "/users-list-app/index.html"
+  },
+  {
+    "revision": "40995044cc9e7cd9658e",
+    "url": "/users-list-app/static/css/2.1ab5750d.chunk.css"
+  },
+  {
+    "revision": "40995044cc9e7cd9658e",
+    "url": "/users-list-app/static/js/2.ccf7cfa1.chunk.js"
+  },
+  {
+    "revision": "723e8e89b7d088a23222",
+    "url": "/users-list-app/static/js/main.2993cfc6.chunk.js"
+  },
+  {
+    "revision": "329802adeed835ddeca9",
+    "url": "/users-list-app/static/js/runtime~main.b494816e.js"
+  }
+]);
